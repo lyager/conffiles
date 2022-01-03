a289d384901c5470715dce258e26c7a6cb7a0436
Jesper L. Nielsen <lyager@gmail.com>
2022-01-03 09:26:31 +0100

    OpenVPN start/stop scripts for osx.

--- openvpn/secomea-down.sh
+++ openvpn/secomea-down.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 rm -Rf /etc/resolver/dev.secomea.com
+rm -Rf /etc/resolver/test.secomea.com
 dscacheutil -flushcache
 exit 0
--- openvpn/secomea-up.sh
+++ openvpn/secomea-up.sh
@@ -7,3 +7,12 @@ nameserver 10.150.30.37
 nameserver 10.150.1.1
 timeout 10
 EOF
+
+cat >/etc/resolver/test.secomea.com  <<EOF
+nameserver 10.160.30.2 
+timeout 10
+EOF
+
+dscacheutil -flushcache
+exit 0
+
